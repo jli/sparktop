7430b206235edaee7d05a3b0c3f18eed6ea3b4fd
john li <jli@circularly.org>
2021-08-23 03:24:42 -0400

    WIP: ugh dont love it, but working...

--- src/view.rs
+++ src/view.rs
@@ -2,9 +2,9 @@
 use anyhow::Result;
 use ordered_float::OrderedFloat as OrdFloat;
 
-use crossterm::event::{KeyCode, KeyEvent};
+use crossterm::event::KeyEvent;
 use tui::{
-    layout::{Constraint, Layout},
+    layout::{Alignment, Constraint, Layout},
     style::{Modifier, Style},
     text::Span,
     widgets::{Block, Borders, Paragraph, Row, Table},
@@ -12,9 +12,9 @@ use tui::{
 
 use crate::{
     event::Next,
+    sproc::SProc,
     sterm::STerm,
-    view_state::{Dir, Metric, ViewState},
-    {render, sproc::SProc},
+    view_state::{Dir, DisplayedColumns, SortColumn, ViewDisplayColumn, ViewState},
 };
 
 #[derive(Default)]
@@ -27,12 +27,12 @@ impl View {
     fn sort(&self, sprocs: &mut Vec<&SProc>) {
         sprocs.sort_by_key(|&sp| {
             let val = match self.state.sort_by {
-                Metric::Pid => sp.pid as f64,
-                Metric::Cpu => sp.cpu_ewma,
-                Metric::Mem => sp.mem_mb,
-                Metric::DiskRead => sp.disk_read_ewma,
-                Metric::DiskWrite => sp.disk_write_ewma,
-                Metric::DiskTotal => sp.disk_read_ewma + sp.disk_write_ewma,
+                SortColumn::Pid => sp.pid as f64,
+                SortColumn::Cpu => sp.cpu_ewma,
+                SortColumn::Mem => sp.mem_mb,
+                SortColumn::DiskRead => sp.disk_read_ewma,
+                SortColumn::DiskWrite => sp.disk_write_ewma,
+                SortColumn::DiskTotal => sp.disk_read_ewma + sp.disk_write_ewma,
             };
             match self.state.sort_dir {
                 Dir::Asc => OrdFloat(val),
@@ -41,29 +41,30 @@ impl View {
         });
     }
 
+    // ViewState + KeyEvent -> Option<Action>
+    // ViewState + Action -> ViewState
     pub fn handle_key(&mut self, key: KeyEvent) -> Next {
-        let mut next = Next::Continue;
-        let mut unhandled = false;
-        match key.code {
-            KeyCode::Char('N') => self.state.sort_by = Metric::Pid,
-            KeyCode::Char('M') => self.state.sort_by = Metric::Mem,
-            KeyCode::Char('P') => self.state.sort_by = Metric::Cpu,
-            KeyCode::Char('R') => self.state.sort_by = Metric::DiskRead,
-            KeyCode::Char('W') => self.state.sort_by = Metric::DiskWrite,
-            KeyCode::Char('D') => self.state.sort_by = Metric::DiskTotal,
-            KeyCode::Char('I') => self.state.sort_dir.flip(),
-            KeyCode::Char('q') => next = Next::Quit,
-            KeyCode::Esc => (), // clear alert
-            _ => unhandled = true,
-        }
-
-        if unhandled {
-            self.state.alert = Some(format!("unhandled key: {:?}", key));
+        // let mut next = Next::Continue;
+        self.state.handle_key(key);
+        if self.state.should_quit {
+            Next::Quit
         } else {
-            self.state.alert = None;
+            Next::Continue
         }
+        // match key.code {
+        //     KeyCode::Char('N') => self.state.sort_by = Metric::Pid,
+        //     KeyCode::Char('M') => self.state.sort_by = Metric::Mem,
+        //     KeyCode::Char('P') => self.state.sort_by = Metric::Cpu,
+        //     KeyCode::Char('R') => self.state.sort_by = Metric::DiskRead,
+        //     KeyCode::Char('W') => self.state.sort_by = Metric::DiskWrite,
+        //     KeyCode::Char('D') => self.state.sort_by = Metric::DiskTotal,
+        //     KeyCode::Char('I') => self.state.sort_dir.flip(),
+        //     KeyCode::Char('q') => next = Next::Quit,
+        //     KeyCode::Esc => (), // clear alert
+        //     _ => unhandled = true,
+        // }
 
-        next
+        // next
     }
 
     pub fn draw(&mut self, sprocs: &mut Vec<&SProc>) -> Result<()> {
@@ -71,110 +72,182 @@ impl View {
         // erhm, borrow checker workarounds...
         let alert = self.state.alert.clone();
         let sort_by = self.state.sort_by;
+        let display_columns = self.state.displayed_columns;
+        let footer = self.state.footer();
         self.terminal.draw(|f| {
+            let full = f.size();
             let main_constraints = if alert.is_some() {
-                vec![Constraint::Min(1), Constraint::Min(3)]
+                vec![
+                    Constraint::Length(full.height - 4),
+                    Constraint::Length(3),
+                    Constraint::Length(1),
+                ]
             } else {
-                vec![Constraint::Percentage(100)]
+                vec![Constraint::Length(full.height - 1), Constraint::Length(1)]
             };
-            let rects = Layout::default()
-                .constraints(main_constraints)
-                .split(f.size());
+            let rects = Layout::default().constraints(main_constraints).split(full);
 
-            // Draw main panel.
-            let main = rects[0];
-            let proc_table = ProcTable::new(sprocs, sort_by);
-            f.render_widget(proc_table.get_table(), main);
+            // Draw process list.
+            let table_area = rects[0];
+            // let proc_table = ProcTable::new(sprocs, sort_by, display_columns);
+            // TODO: lifetime hacks...
+            let vdcols = display_columns.shown();
+            let constraints: Vec<Constraint> = vdcols
+                .iter()
+                .map(|ViewDisplayColumn(_, _, _, _, constraint)| constraint)
+                .copied()
+                .collect();
+            let proc_table = ProcTable::build(sprocs, sort_by, display_columns, &constraints);
+            f.render_widget(proc_table, table_area);
 
             // Draw alert.
             if let Some(alert) = alert {
                 let msg = Paragraph::new(alert).block(Block::default().borders(Borders::ALL));
                 f.render_widget(msg, rects[1])
             }
+
+            // Draw help footer.
+            let footer_area = rects[rects.len() - 1];
+            f.render_widget(
+                Paragraph::new(Span::styled(
+                    footer,
+                    Style::default().add_modifier(Modifier::UNDERLINED),
+                ))
+                .alignment(Alignment::Right),
+                footer_area,
+            );
         })?;
         Ok(())
     }
 }
 
-struct ProcTable<'a> {
-    header: Vec<String>,
-    sprocs: &'a [&'a SProc],
-}
+// struct ProcTable<'a> {
+//     header: Vec<String>,
+//     sprocs: &'a [&'a SProc],
+// }
+// TODO: make this a Widget
+struct ProcTable();
 
-impl<'a> ProcTable<'a> {
-    fn new(sprocs: &'a [&SProc], sort_by: Metric) -> Self {
-        use Metric::*;
-        let mut header = vec![String::from("pid"), String::from("process")];
-        header.extend(
-            [DiskRead, DiskWrite, Mem, Cpu]
-                .iter()
-                .map(|m| m.to_header_str(sort_by)),
-        );
-        header.push(String::from("cpu history"));
-        Self { header, sprocs }
-    }
-
-    fn get_table(&self) -> impl tui::widgets::Widget + '_ {
-        let rows = self.sprocs.iter().map(|sp| {
-            let mut liveness_style = Style::default();
-            if sp.is_dead() {
-                liveness_style = liveness_style.fg(tui::style::Color::Red);
-            }
-            Row::new(
-                vec![
-                    Span::styled(sp.pid.to_string(), liveness_style),
-                    Span::styled(sp.name.clone(), liveness_style),
-                    Span::from(render_metric(sp.disk_read_ewma)),
-                    Span::from(render_metric(sp.disk_write_ewma)),
-                    Span::from(render_metric(sp.mem_mb)),
-                    Span::from(render_metric(sp.cpu_ewma)),
-                    Span::from(render::render_vec(&sp.cpu_hist, 100.)),
-                ]
-                .into_iter(),
-            )
+impl ProcTable {
+    fn build<'a>(
+        sprocs: &'a [&SProc],
+        sort_by: SortColumn,
+        display_columns: DisplayedColumns,
+        constraints: &'a [Constraint],
+    ) -> impl tui::widgets::Widget + 'a {
+        // use DisplayColumn::*;
+        let header = display_columns.header(&sort_by);
+        // let vdcols = display_columns.shown();
+        let rows = sprocs.iter().map(|sp| {
+            // let mut liveness_style = Style::default();
+            // if sp.is_dead() {
+            //     liveness_style = liveness_style.fg(Color::Red);
+            // }
+            // let values = vdcols.map(|ViewDisplayColumn(c, _, _, _)| match c {
+            //     Pid => Span::styled(sp.pid.to_string(), liveness_style),
+            //     ProcessName => Span::styled(sp.name.clone(), liveness_style),
+            //     DiskRead => Span::from(render_metric(sp.disk_read_ewma)),
+            //     DiskWrite => Span::from(render_metric(sp.disk_write_ewma)),
+            //     Mem => Span::from(render_metric(sp.mem_mb)),
+            //     Cpu => Span::from(render_metric(sp.cpu_ewma)),
+            //     CpuHistory => Span::from(render::render_vec(&sp.cpu_hist, 100.)),
+            // });
+            let values = display_columns.row_data(sp);
+            Row::new(values)
         });
+        // let constraints: Vec<Constraint> = vdcols.iter().map(|ViewDisplayColumn(_,_,_,_,constraint)| constraint).copied().collect();
         Table::new(rows)
             .header(
                 // TODO: way to avoid making copy?
-                Row::new(self.header.to_vec())
+                Row::new(header.to_vec())
                     .style(Style::default().add_modifier(Modifier::UNDERLINED)),
             )
-            .widths(&[
-                Constraint::Length(6),
-                Constraint::Length(24),
-                Constraint::Length(5),
-                Constraint::Length(5),
-                Constraint::Length(5),
-                Constraint::Length(4),
-                Constraint::Percentage(100),
-            ])
+            .widths(constraints)
+        // .widths(&[
+        //     Constraint::Length(6),
+        //     Constraint::Length(24),
+        //     Constraint::Length(5),
+        //     Constraint::Length(5),
+        //     Constraint::Length(5),
+        //     Constraint::Length(4),
+        //     Constraint::Percentage(100),
+        // ])
     }
+    // fn new(sprocs: &'a [&SProc], sort_by: SortColumn, display_columns: DisplayedColumns) -> Self {
+    //     // use SortColumn as S;
+    //     // let mut header = vec![String::from("pid"), String::from("process")];
+    //     // header.extend(
+    //     //     [S.DiskRead, S.DiskWrite, S.Mem, S.Cpu]
+    //     //         .iter()
+    //     //         .map(|m| m.to_header_str(sort_by)),
+    //     // );
+    //     // header.push(String::from("cpu history"));
+    //     // Self { header, sprocs }
+    //     let header = display_columns.header(&sort_by);
+    //     Self { header, sprocs }
+    // }
+
+    // fn get_table(&self) -> impl tui::widgets::Widget + '_ {
+    //     let rows = self.sprocs.iter().map(|sp| {
+    //         let mut liveness_style = Style::default();
+    //         if sp.is_dead() {
+    //             liveness_style = liveness_style.fg(Color::Red);
+    //         }
+    //         Row::new(
+    //             vec![
+    //                 Span::styled(sp.pid.to_string(), liveness_style),
+    //                 Span::styled(sp.name.clone(), liveness_style),
+    //                 Span::from(render_metric(sp.disk_read_ewma)),
+    //                 Span::from(render_metric(sp.disk_write_ewma)),
+    //                 Span::from(render_metric(sp.mem_mb)),
+    //                 Span::from(render_metric(sp.cpu_ewma)),
+    //                 Span::from(render::render_vec(&sp.cpu_hist, 100.)),
+    //             ]
+    //             .into_iter(),
+    //         )
+    //     });
+    //     Table::new(rows)
+    //         .header(
+    //             // TODO: way to avoid making copy?
+    //             Row::new(self.header.to_vec())
+    //                 .style(Style::default().add_modifier(Modifier::UNDERLINED)),
+    //         )
+    //         .widths(&[
+    //             Constraint::Length(6),
+    //             Constraint::Length(24),
+    //             Constraint::Length(5),
+    //             Constraint::Length(5),
+    //             Constraint::Length(5),
+    //             Constraint::Length(4),
+    //             Constraint::Percentage(100),
+    //         ])
+    // }
 }
 
-impl Metric {
-    fn to_header_str(self, sort_by: Metric) -> String {
-        use Metric::*;
-        let s = match self {
-            Pid => "pid",
-            Cpu => "cpu",
-            Mem => "mem",
-            DiskRead => "dr",
-            DiskWrite => "dw",
-            DiskTotal => "d+",
-        };
-        if sort_by == self || (sort_by == DiskTotal && (self == DiskRead || self == DiskWrite)) {
-            format!("*{}*", s)
-        } else {
-            String::from(s)
-        }
-    }
-}
+// impl SortColumn {
+//     fn to_header_str(self, sort_by: SortColumn) -> String {
+//         use SortColumn::*;
+//         let s = match self {
+//             Pid => "pid",
+//             Cpu => "cpu",
+//             Mem => "mem",
+//             DiskRead => "dr",
+//             DiskWrite => "dw",
+//             DiskTotal => "d+",
+//         };
+//         if sort_by == self || (sort_by == DiskTotal && (self == DiskRead || self == DiskWrite)) {
+//             format!("*{}*", s)
+//         } else {
+//             String::from(s)
+//         }
+//     }
+// }
 
 // hide low values
-fn render_metric(m: f64) -> String {
-    if m < 0.05 {
-        String::from("_")
-    } else {
-        format!("{:.1}", m)
-    }
-}
+// fn render_metric(m: f64) -> String {
+//     if m < 0.05 {
+//         String::from("_")
+//     } else {
+//         format!("{:.1}", m)
+//     }
+// }
--- src/view_state.rs
+++ src/view_state.rs
@@ -1,31 +1,363 @@
 /// ViewState: view model and interactions.
 // rendering is done in view.rs
+use crossterm::event::{KeyCode, KeyEvent};
+use tui::layout::Constraint;
+
+use crate::{render, sproc::SProc};
 
 pub struct ViewState {
-    pub sort_by: Metric,
+    pub sort_by: SortColumn,
     pub sort_dir: Dir,
+    pub displayed_columns: DisplayedColumns,
     pub alert: Option<String>,
+    pub should_quit: bool,
+    action: Action,
 }
 
 impl Default for ViewState {
     fn default() -> Self {
         Self {
-            sort_by: Metric::Cpu,
+            sort_by: SortColumn::Cpu,
             sort_dir: Dir::Desc,
+            displayed_columns: DisplayedColumns::default(),
             alert: None,
+            should_quit: false,
+            action: Action::Top,
+        }
+    }
+}
+
+impl ViewState {
+    pub fn handle_key(&mut self, key_event: KeyEvent) {
+        let code = key_event.code;
+        use Action::*;
+        let mut unhandled = false;
+        match (&self.action, code) {
+            (_, KeyCode::Esc) => self.action = Top,
+            (&Top, KeyCode::Char('q')) => self.should_quit = true,
+            (&Top, KeyCode::Char(c)) => match Action::action_from_char(c) {
+                Some(a) => self.action = a,
+                None => unhandled = true,
+            },
+            (&SelectSort, KeyCode::Char(c)) => match Action::sort_col_from_char(c) {
+                Some(col) => {
+                    self.sort_by = col;
+                    self.action = Top;
+                }
+                None => unhandled = true,
+            },
+            (&ToggleColumn, KeyCode::Char(c)) => match Action::display_col_from_char(c) {
+                Some(col) => {
+                    self.displayed_columns.toggle(col);
+                    self.action = Top;
+                }
+                None => unhandled = true,
+            },
+            _ => unhandled = true,
+        };
+
+        self.alert = if unhandled {
+            Some(format!("unhandled key: {:?}", key_event))
+        } else {
+            None
+        };
+    }
+
+    pub fn footer(&self) -> String {
+        match &self.action {
+            Action::Top => Action::action_help(),
+            Action::SelectSort => Action::sort_col_help(),
+            Action::ToggleColumn => Action::display_col_help(),
         }
     }
 }
 
 #[derive(Copy, Clone, PartialEq)]
-pub enum Metric {
-    Pid, // not really a "metric"... rename this?
+pub enum Action {
+    Top,
+    SelectSort,
+    ToggleColumn,
+}
+
+impl Action {
+    fn action_from_char(input_c: char) -> Option<Action> {
+        VIEW_ACTIONS
+            .iter()
+            .find_map(|ViewAction(a, c, _)| if input_c == *c { Some(*a) } else { None })
+    }
+
+    fn action_help() -> String {
+        // note: can use iterator::intersperse when it's stable
+        let parts: Vec<&str> = VIEW_ACTIONS
+            .iter()
+            .map(|ViewAction(_, _, help)| help)
+            .copied()
+            .collect();
+        parts.join("  ")
+    }
+
+    fn sort_col_from_char(input_c: char) -> Option<SortColumn> {
+        VIEW_SORT_COLUMNS
+            .iter()
+            .find_map(|ViewSortColumn(sc, c, _)| if input_c == *c { Some(*sc) } else { None })
+    }
+
+    fn sort_col_help() -> String {
+        let parts: Vec<&str> = VIEW_SORT_COLUMNS
+            .iter()
+            .map(|ViewSortColumn(_, _, help)| help)
+            .copied()
+            .collect();
+        parts.join("  ")
+    }
+
+    fn display_col_from_char(input_c: char) -> Option<DisplayColumn> {
+        VIEW_DISPLAY_COLUMNS.iter().find_map(
+            |ViewDisplayColumn(dc, c, _, _, _)| if input_c == *c { Some(*dc) } else { None },
+        )
+    }
+
+    fn display_col_help() -> String {
+        let parts: Vec<&str> = VIEW_DISPLAY_COLUMNS
+            .iter()
+            .map(|ViewDisplayColumn(_, _, help, _, _)| help)
+            .copied()
+            .collect();
+        parts.join("  ")
+    }
+}
+
+struct ViewAction(Action, char, &'static str);
+
+const VIEW_ACTIONS: [ViewAction; 2] = [
+    ViewAction(Action::SelectSort, 's', "(s)ort"),
+    ViewAction(Action::ToggleColumn, 'c', "(c)olumns"),
+];
+
+#[derive(Clone, Copy)]
+pub enum DisplayColumn {
+    Pid,
+    ProcessName,
+    DiskRead,
+    DiskWrite,
+    Mem,
+    Cpu,
+    CpuHistory,
+}
+
+// column, action char, action help, column name
+// #[derive(Clone, Copy)]
+pub struct ViewDisplayColumn(
+    pub DisplayColumn,
+    pub char,
+    pub &'static str,
+    pub &'static str,
+    pub Constraint,
+);
+
+const VIEW_DISPLAY_COLUMNS: [ViewDisplayColumn; 7] = [
+    ViewDisplayColumn(
+        DisplayColumn::Pid,
+        'p',
+        "(p)id",
+        "pid",
+        Constraint::Length(6),
+    ),
+    ViewDisplayColumn(
+        DisplayColumn::ProcessName,
+        'n',
+        "process-(n)ame",
+        "process",
+        Constraint::Length(24),
+    ),
+    ViewDisplayColumn(
+        DisplayColumn::DiskRead,
+        'r',
+        "disk-(r)ead",
+        "dr",
+        Constraint::Length(5),
+    ),
+    ViewDisplayColumn(
+        DisplayColumn::DiskWrite,
+        'w',
+        "diks-(w)rite",
+        "dw",
+        Constraint::Length(5),
+    ),
+    ViewDisplayColumn(
+        DisplayColumn::Mem,
+        'm',
+        "(m)em",
+        "mem",
+        Constraint::Length(5),
+    ),
+    ViewDisplayColumn(
+        DisplayColumn::Cpu,
+        'c',
+        "(c)pu",
+        "cpu",
+        Constraint::Length(4),
+    ),
+    ViewDisplayColumn(
+        DisplayColumn::CpuHistory,
+        'h',
+        "cpu-(h)istory",
+        "cpu history",
+        Constraint::Percentage(100),
+    ),
+];
+
+// hmmm, maybe just map from DisplayColumn to bool?
+#[derive(Clone, Copy)]
+pub struct DisplayedColumns {
+    pid: bool,
+    process_name: bool,
+    disk_read: bool,
+    disk_write: bool,
+    mem: bool,
+    cpu: bool,
+    cpu_history: bool,
+}
+
+impl Default for DisplayedColumns {
+    fn default() -> Self {
+        Self {
+            pid: true,
+            process_name: true,
+            disk_read: true,
+            disk_write: true,
+            mem: true,
+            cpu: true,
+            cpu_history: true,
+        }
+    }
+}
+
+impl DisplayedColumns {
+    fn toggle(&mut self, col: DisplayColumn) {
+        use DisplayColumn::*;
+        match col {
+            Pid => self.pid = !self.pid,
+            ProcessName => self.process_name = !self.process_name,
+            DiskRead => self.disk_read = !self.disk_read,
+            DiskWrite => self.disk_write = !self.disk_write,
+            Mem => self.mem = !self.mem,
+            Cpu => self.cpu = !self.cpu,
+            CpuHistory => self.cpu_history = !self.cpu_history,
+        }
+    }
+
+    fn should_show(&self, col: &DisplayColumn) -> bool {
+        match col {
+            DisplayColumn::Pid => self.pid,
+            DisplayColumn::ProcessName => self.process_name,
+            DisplayColumn::DiskRead => self.disk_read,
+            DisplayColumn::DiskWrite => self.disk_write,
+            DisplayColumn::Mem => self.mem,
+            DisplayColumn::Cpu => self.cpu,
+            DisplayColumn::CpuHistory => self.cpu_history,
+        }
+    }
+
+    pub fn shown(&self) -> Vec<&ViewDisplayColumn> {
+        VIEW_DISPLAY_COLUMNS
+            .iter()
+            .filter(|ViewDisplayColumn(dc, _, _, _, _)| self.should_show(dc))
+            .collect()
+    }
+
+    pub fn header(&self, sort_by: &SortColumn) -> Vec<String> {
+        VIEW_DISPLAY_COLUMNS
+            .iter()
+            .filter_map(|ViewDisplayColumn(dc, _, _, h, _)| {
+                if self.should_show(dc) { Some(h) } else { None }.map(|h| {
+                    if sort_by == dc {
+                        format!("*{}*", h)
+                    } else {
+                        String::from(*h)
+                    }
+                })
+            })
+            .collect()
+    }
+
+    // TODO: gurgh, why can't i just do this in view.rs? oh, maybe because i forgot .iter()..
+    pub fn row_data(&self, sp: &SProc) -> Vec<String> {
+        use DisplayColumn::*;
+        VIEW_DISPLAY_COLUMNS
+            .iter()
+            .filter_map(|ViewDisplayColumn(dc, _, _, _, _)| {
+                if !self.should_show(dc) {
+                    return None;
+                }
+                Some(match dc {
+                    Pid => sp.pid.to_string(),
+                    ProcessName => sp.name.clone(),
+                    DiskRead => render_metric(sp.disk_read_ewma),
+                    DiskWrite => render_metric(sp.disk_write_ewma),
+                    Mem => render_metric(sp.mem_mb),
+                    Cpu => render_metric(sp.cpu_ewma),
+                    CpuHistory => render::render_vec(&sp.cpu_hist, 100.),
+                })
+            })
+            .collect()
+    }
+}
+
+// hide low values
+fn render_metric(m: f64) -> String {
+    if m < 0.05 {
+        String::from("_")
+    } else {
+        format!("{:.1}", m)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum SortColumn {
+    Pid,
     Cpu,
     Mem,
     DiskRead,
     DiskWrite,
     DiskTotal,
 }
+
+impl PartialEq<DisplayColumn> for SortColumn {
+    fn eq(&self, other: &DisplayColumn) -> bool {
+        use DisplayColumn as D;
+        use SortColumn as S;
+        matches!(
+            (self, other),
+            (S::Pid, D::Pid)
+                | (S::Cpu, D::Cpu)
+                | (S::Mem, D::Mem)
+                | (S::DiskRead, D::DiskRead)
+                | (S::DiskWrite, D::DiskWrite)
+                | (S::DiskTotal, D::DiskWrite | D::DiskRead)
+        )
+        // match (self, other) {
+        //     (S::Pid, D::Pid)
+        //     | (S::Cpu, D::Cpu)
+        //     | (S::Mem, D::Mem)
+        //     | (S::DiskRead, D::DiskRead)
+        //     | (S::DiskWrite, D::DiskWrite)
+        //     | (S::DiskTotal, D::DiskWrite | D::DiskRead) => true,
+        //     _ => false,
+        // }
+    }
+}
+
+struct ViewSortColumn(SortColumn, char, &'static str);
+
+const VIEW_SORT_COLUMNS: [ViewSortColumn; 6] = [
+    ViewSortColumn(SortColumn::Pid, 'p', "(p)id"),
+    ViewSortColumn(SortColumn::DiskRead, 'r', "disk-(r)ead"),
+    ViewSortColumn(SortColumn::DiskWrite, 'w', "disk-(w)rite"),
+    ViewSortColumn(SortColumn::DiskTotal, 'd', "(d)isk-total"),
+    ViewSortColumn(SortColumn::Mem, 'm', "(m)em"),
+    ViewSortColumn(SortColumn::Cpu, 'c', "(c)pu"),
+];
 
 pub enum Dir {
     Asc,
